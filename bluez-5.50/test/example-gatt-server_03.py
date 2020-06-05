02051b4feeebf89d5cd447b18adaa42ff0497123
lubusax <lu.bu.sax@gmail.com>
2020-06-06 00:50:27 +0200

    autosave

--- bluez-5.50/test/example-gatt-server_03.py
+++ bluez-5.50/test/example-gatt-server_03.py
@@ -431,184 +431,184 @@ class BatteryLevelCharacteristic(Characteristic):
         self.notifying = False
 
 
-# class TestService(Service):
-    #     """
-    #     Dummy test service that provides characteristics and descriptors that
-    #     exercise various API functionality.
+class TestService(Service):
+    """
+    Dummy test service that provides characteristics and descriptors that
+    exercise various API functionality.
 
-    #     """
-    #     TEST_SVC_UUID = '12345678-1234-5678-1234-56789abcdef0'
+    """
+    TEST_SVC_UUID = '12345678-1234-5678-1234-56789abcdef0'
 
-    #     def __init__(self, bus, index):
-    #         Service.__init__(self, bus, index, self.TEST_SVC_UUID, True)
-    #         self.add_characteristic(TestCharacteristic(bus, 0, self))
-    #         self.add_characteristic(TestEncryptCharacteristic(bus, 1, self))
-    #         self.add_characteristic(TestSecureCharacteristic(bus, 2, self))
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.TEST_SVC_UUID, True)
+        self.add_characteristic(TestCharacteristic(bus, 0, self))
+        self.add_characteristic(TestEncryptCharacteristic(bus, 1, self))
+        self.add_characteristic(TestSecureCharacteristic(bus, 2, self))
 
-    # class TestCharacteristic(Characteristic):
-    #     """
-    #     Dummy test characteristic. Allows writing arbitrary bytes to its value, and
-    #     contains "extended properties", as well as a test descriptor.
+class TestCharacteristic(Characteristic):
+    """
+    Dummy test characteristic. Allows writing arbitrary bytes to its value, and
+    contains "extended properties", as well as a test descriptor.
 
-    #     """
-    #     TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef1'
+    """
+    TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef1'
 
-    #     def __init__(self, bus, index, service):
-    #         Characteristic.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_CHRC_UUID,
-    #                 ['read', 'write', 'writable-auxiliaries'],
-    #                 service)
-    #         self.value = []
-    #         self.add_descriptor(TestDescriptor(bus, 0, self))
-    #         self.add_descriptor(
-    #                 CharacteristicUserDescriptionDescriptor(bus, 1, self))
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.TEST_CHRC_UUID,
+                ['read', 'write', 'writable-auxiliaries'],
+                service)
+        self.value = []
+        self.add_descriptor(TestDescriptor(bus, 0, self))
+        self.add_descriptor(
+                CharacteristicUserDescriptionDescriptor(bus, 1, self))
 
-    #     def ReadValue(self, options):
-    #         print('TestCharacteristic Read: ' + repr(self.value))
-    #         return self.value
+    def ReadValue(self, options):
+        print('TestCharacteristic Read: ' + repr(self.value))
+        return self.value
 
-    #     def WriteValue(self, value, options):
-    #         print('TestCharacteristic Write: ' + repr(value))
-    #         self.value = value
-
-
-    # class TestDescriptor(Descriptor):
-    #     """
-    #     Dummy test descriptor. Returns a static value.
-
-    #     """
-    #     TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef2'
-
-    #     def __init__(self, bus, index, characteristic):
-    #         Descriptor.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_DESC_UUID,
-    #                 ['read', 'write'],
-    #                 characteristic)
-
-    #     def ReadValue(self, options):
-    #         return [
-    #                 dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
-    #         ]
+    def WriteValue(self, value, options):
+        print('TestCharacteristic Write: ' + repr(value))
+        self.value = value
 
 
-    # class CharacteristicUserDescriptionDescriptor(Descriptor):
-    #     """
-    #     Writable CUD descriptor.
+class TestDescriptor(Descriptor):
+    """
+    Dummy test descriptor. Returns a static value.
 
-    #     """
-    #     CUD_UUID = '2901'
+    """
+    TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef2'
 
-    #     def __init__(self, bus, index, characteristic):
-    #         self.writable = 'writable-auxiliaries' in characteristic.flags
-    #         self.value = array.array('B', b'This is a characteristic for testing')
-    #         self.value = self.value.tolist()
-    #         Descriptor.__init__(
-    #                 self, bus, index,
-    #                 self.CUD_UUID,
-    #                 ['read', 'write'],
-    #                 characteristic)
+    def __init__(self, bus, index, characteristic):
+        Descriptor.__init__(
+                self, bus, index,
+                self.TEST_DESC_UUID,
+                ['read', 'write'],
+                characteristic)
 
-    #     def ReadValue(self, options):
-    #         return self.value
-
-    #     def WriteValue(self, value, options):
-    #         if not self.writable:
-    #             raise NotPermittedException()
-    #         self.value = value
-
-    # class TestEncryptCharacteristic(Characteristic):
-    #     """
-    #     Dummy test characteristic requiring encryption.
-
-    #     """
-    #     TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef3'
-
-    #     def __init__(self, bus, index, service):
-    #         Characteristic.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_CHRC_UUID,
-    #                 ['encrypt-read', 'encrypt-write'],
-    #                 service)
-    #         self.value = []
-    #         self.add_descriptor(TestEncryptDescriptor(bus, 2, self))
-    #         self.add_descriptor(
-    #                 CharacteristicUserDescriptionDescriptor(bus, 3, self))
-
-    #     def ReadValue(self, options):
-    #         print('TestEncryptCharacteristic Read: ' + repr(self.value))
-    #         return self.value
-
-    #     def WriteValue(self, value, options):
-    #         print('TestEncryptCharacteristic Write: ' + repr(value))
-    #         self.value = value
-
-    # class TestEncryptDescriptor(Descriptor):
-    #     """
-    #     Dummy test descriptor requiring encryption. Returns a static value.
-
-    #     """
-    #     TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef4'
-
-    #     def __init__(self, bus, index, characteristic):
-    #         Descriptor.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_DESC_UUID,
-    #                 ['encrypt-read', 'encrypt-write'],
-    #                 characteristic)
-
-    #     def ReadValue(self, options):
-    #         return [
-    #                 dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
-    #         ]
+    def ReadValue(self, options):
+        return [
+                dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
+        ]
 
 
-    # class TestSecureCharacteristic(Characteristic):
-    #     """
-    #     Dummy test characteristic requiring secure connection.
+class CharacteristicUserDescriptionDescriptor(Descriptor):
+    """
+    Writable CUD descriptor.
 
-    #     """
-    #     TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef5'
+    """
+    CUD_UUID = '2901'
 
-    #     def __init__(self, bus, index, service):
-    #         Characteristic.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_CHRC_UUID,
-    #                 ['secure-read', 'secure-write'],
-    #                 service)
-    #         self.value = []
-    #         self.add_descriptor(TestSecureDescriptor(bus, 2, self))
-    #         self.add_descriptor(
-    #                 CharacteristicUserDescriptionDescriptor(bus, 3, self))
+    def __init__(self, bus, index, characteristic):
+        self.writable = 'writable-auxiliaries' in characteristic.flags
+        self.value = array.array('B', b'This is a characteristic for testing')
+        self.value = self.value.tolist()
+        Descriptor.__init__(
+                self, bus, index,
+                self.CUD_UUID,
+                ['read', 'write'],
+                characteristic)
 
-    #     def ReadValue(self, options):
-    #         print('TestSecureCharacteristic Read: ' + repr(self.value))
-    #         return self.value
+    def ReadValue(self, options):
+        return self.value
 
-    #     def WriteValue(self, value, options):
-    #         print('TestSecureCharacteristic Write: ' + repr(value))
-    #         self.value = value
+    def WriteValue(self, value, options):
+        if not self.writable:
+            raise NotPermittedException()
+        self.value = value
+
+class TestEncryptCharacteristic(Characteristic):
+    """
+    Dummy test characteristic requiring encryption.
+
+    """
+    TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef3'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.TEST_CHRC_UUID,
+                ['encrypt-read', 'encrypt-write'],
+                service)
+        self.value = []
+        self.add_descriptor(TestEncryptDescriptor(bus, 2, self))
+        self.add_descriptor(
+                CharacteristicUserDescriptionDescriptor(bus, 3, self))
+
+    def ReadValue(self, options):
+        print('TestEncryptCharacteristic Read: ' + repr(self.value))
+        return self.value
+
+    def WriteValue(self, value, options):
+        print('TestEncryptCharacteristic Write: ' + repr(value))
+        self.value = value
+
+class TestEncryptDescriptor(Descriptor):
+    """
+    Dummy test descriptor requiring encryption. Returns a static value.
+
+    """
+    TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef4'
+
+    def __init__(self, bus, index, characteristic):
+        Descriptor.__init__(
+                self, bus, index,
+                self.TEST_DESC_UUID,
+                ['encrypt-read', 'encrypt-write'],
+                characteristic)
+
+    def ReadValue(self, options):
+        return [
+                dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
+        ]
 
 
-    # class TestSecureDescriptor(Descriptor):
-    #     """
-    #     Dummy test descriptor requiring secure connection. Returns a static value.
+class TestSecureCharacteristic(Characteristic):
+    """
+    Dummy test characteristic requiring secure connection.
 
-    #     """
-    #     TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef6'
+    """
+    TEST_CHRC_UUID = '12345678-1234-5678-1234-56789abcdef5'
 
-    #     def __init__(self, bus, index, characteristic):
-    #         Descriptor.__init__(
-    #                 self, bus, index,
-    #                 self.TEST_DESC_UUID,
-    #                 ['secure-read', 'secure-write'],
-    #                 characteristic)
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.TEST_CHRC_UUID,
+                ['secure-read', 'secure-write'],
+                service)
+        self.value = []
+        self.add_descriptor(TestSecureDescriptor(bus, 2, self))
+        self.add_descriptor(
+                CharacteristicUserDescriptionDescriptor(bus, 3, self))
 
-    #     def ReadValue(self, options):
-    #         return [
-    #                 dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
-    #         ]
+    def ReadValue(self, options):
+        print('TestSecureCharacteristic Read: ' + repr(self.value))
+        return self.value
+
+    def WriteValue(self, value, options):
+        print('TestSecureCharacteristic Write: ' + repr(value))
+        self.value = value
+
+
+class TestSecureDescriptor(Descriptor):
+    """
+    Dummy test descriptor requiring secure connection. Returns a static value.
+
+    """
+    TEST_DESC_UUID = '12345678-1234-5678-1234-56789abcdef6'
+
+    def __init__(self, bus, index, characteristic):
+        Descriptor.__init__(
+                self, bus, index,
+                self.TEST_DESC_UUID,
+                ['secure-read', 'secure-write'],
+                characteristic)
+
+    def ReadValue(self, options):
+        return [
+                dbus.Byte('T'), dbus.Byte('e'), dbus.Byte('s'), dbus.Byte('t')
+        ]
 
 def register_app_cb():
     print('GATT application registered')
